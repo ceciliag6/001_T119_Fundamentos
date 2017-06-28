b45d08b7094a50940b6fa2dea620cc49952ac531
cecilia <cecilia@192.168.2.2>
2017-06-28 17:38:41 +0200

    (Initial Commit)

--- src/com/cice/fundamentos/Arreglos.java
+++ src/com/cice/fundamentos/Arreglos.java
@@ -28,16 +28,28 @@ public class Arreglos {
         int nombres[][];
         
         int[] edades[] = new int[2][2];
-        String[] tipos[][] = new String[2][2][2];
+        int[] tipos[][] = new int[2][2][2];
         
-        tipos[0][0][1] = "25";
+        tipos[0][0][1] = 2;
         
         //[3][3][6]
         //tresd[0][0][2] = 0;
         
-        for (String[][] c : tipos) {
-            for(String[] d : c) {
-                for(String v : d){
+     /*   
+        for(int[][] mi_array_2d:tresd){
+            for (int[] mi_arr:mi_array_2d ){
+                for(int variable:mi_arr){
+                    System.out.println(variable); 
+                }
+            }
+        }
+        
+        
+     */   
+        
+        for (int[][] c : tipos) {
+            for(int[] d : c) {
+                for(int v : d){
                     System.out.println(v);
                 }
             }
